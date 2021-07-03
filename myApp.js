f540f6df91d60162dece0355a7ab437c19a2cc6d
Wendy Kelly <wendykelly@Wendys-iMac.local>
2021-07-03 07:44:34 -0700

    first commit

--- myApp.js
+++ myApp.js
@@ -1,5 +1,11 @@
-var express = require('express');
-var app = express();
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
 
 
 
